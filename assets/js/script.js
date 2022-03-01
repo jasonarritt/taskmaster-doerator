90c198065df9f75d922f7edff67594b47ef4120f
Jason Arritt <jsn.arritt@gmail.com>
2022-03-01 11:39:48 -0500

    to do items added to list with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
+//Handler for creating tasks
 function createTaskHandler(event) {
+    //Prevent default behavior for events
     event.preventDefault();
+
+    //Variables
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //Create div to contain task and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //Add list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
